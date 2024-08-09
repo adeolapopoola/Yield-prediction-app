6485bb4fbf3231da7d7a3e1780dccfea38d51236
Adeola Popoola <165803492+adeolapopoola@users.noreply.github.com>
2024-08-09 20:08:46 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -5,7 +5,7 @@ def app():
         """
         <style>
         [data-testid = 'stAppViewContainer']{
-        background-image:url('Agri-LL_Square.jpg');
+        background-image:url('https://img.freepik.com/premium-photo/hand-robotic-holding-plant-dirt-generative-ai-planting-trees-reforestation_687553-6976.jpg?w=1800');
         background-size: 95%;
         background-position: center;
         background-repeat: no-repeat;
