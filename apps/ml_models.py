e1b3d8967e2383d0b7ec94c5f3d03f50a3a1291d
Adeola Popoola <165803492+adeolapopoola@users.noreply.github.com>
2024-08-09 18:49:11 +0200

    Update ml_models.py

--- apps/ml_models.py
+++ apps/ml_models.py
@@ -8,10 +8,12 @@ timeseries_df = pd.read_csv('Dashboard_data (1).xls', parse_dates=['Year'])
 timeseries_df.set_index('Year', inplace=True)
 
 xgboost_model = pickle.load(open('boost.pkl', 'rb'))
+nn_model = pickle.load(open('nn.pkl', 'rb'))
+preprocessor = pickle.load(open('preprocessor.pkl', 'rb'))
 #knr_model = joblib.load('knr.pkl')
 #xgboost_model = joblib.load('boost.pkl')
-nn_model = joblib.load('nn.pkl')
-preprocessor = joblib.load('preprocessor.pkl')
+#nn_model = joblib.load('nn.pkl')
+#preprocessor = joblib.load('preprocessor.pkl')
 
 #Function to calculate country-specific quartiles
 def calculate_country_quartiles(df, country):
