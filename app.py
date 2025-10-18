1edd6ddfc2f228487ce8408722ded3e0003ffb0c
Adeola Popoola <165803492+adeolapopoola@users.noreply.github.com>
2025-10-18 13:41:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from multiapp import MultiApp
 #from apps import home, dashboards, arima, ml_models, about
 apps/
- ├── __init__.py     
+ __init__.py     
  ├── home.py
  ├── dashboards.py
  ├── arima.py
